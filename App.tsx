4275a1fb9b189f322e36cfeaa6b71dd53a8d060c
Milka Vladislav <76705901+milkavladislav@users.noreply.github.com>
2022-04-04 17:34:12 +0300

    do task

--- App.tsx
+++ App.tsx
@@ -1,20 +1,118 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import {
+  Input,
+  IconButton,
+  Checkbox,
+  Text,
+  Box,
+  VStack,
+  HStack,
+  Heading,
+  Icon,
+  Center,
+  useToast,
+  NativeBaseProvider,
+} from "native-base";
+import { useState } from "react";
+import { Feather, Entypo } from "@expo/vector-icons";
+
+const Example = () => {
+  const instState = [{
+    title: "Code",
+    isCompleted: true
+  }, {
+    title: "Meeting with team at 9",
+    isCompleted: false
+  }, {
+    title: "Check Emails",
+    isCompleted: false
+  }, {
+    title: "Write an article",
+    isCompleted: false
+  }];
+  const [list, setList] = useState(instState);
+  const [inputValue, setInputValue] = useState("");
+  const toast = useToast();
+
+  const addItem = (title: string) => {
+    if (title === "") {
+      toast.show({
+        title: "Please Enter Text",
+        status: "warning"
+      });
+      return;
+    }
+
+    setList(prevList => {
+      return [...prevList, {
+        title: title,
+        isCompleted: false
+      }];
+    });
+  };
+
+  const handleDelete = (index: number) => {
+    setList(prevList => {
+      const temp = prevList.filter((_, itemI) => itemI !== index);
+      return temp;
+    });
+  };
+
+  const handleStatusChange = (index: number) => {
+    setList(prevList => {
+      const newList = [...prevList];
+      newList[index].isCompleted = !newList[index].isCompleted;
+      return newList;
+    });
+  };
+
+  return <Center w="100%">
+      <Box maxW="300" w="100%">
+        <Heading mb="2" size="md">
+          Wednesday
+        </Heading>
+        <VStack space={4}>
+          <HStack space={2}>
+            <Input flex={1} onChangeText={v => setInputValue(v)} value={inputValue} placeholder="Add Task" />
+            <IconButton borderRadius="sm" variant="solid" icon={<Icon as={Feather} name="plus" size="sm" color="warmGray.50" />} onPress={() => {
+            addItem(inputValue);
+            setInputValue("");
+          }} />
+          </HStack>
+          <VStack space={2}>
+            {list.map((item, itemI) => <HStack w="100%" justifyContent="space-between" alignItems="center" key={item.title + itemI.toString()}>
+                <Checkbox isChecked={item.isCompleted} onChange={() => handleStatusChange(itemI)} value={item.title}></Checkbox>
+                <Text width="100%" flexShrink={1} textAlign="left" mx="2" strikeThrough={item.isCompleted} _light={{
+              color: item.isCompleted ? "gray.400" : "coolGray.800"
+            }} _dark={{
+              color: item.isCompleted ? "gray.400" : "coolGray.50"
+            }} onPress={() => handleStatusChange(itemI)}>
+                  {item.title}
+                </Text>
+                <IconButton size="sm" colorScheme="trueGray" icon={<Icon as={Entypo} name="minus" size="xs" color="trueGray.400" />} onPress={() => handleDelete(itemI)} />
+              </HStack>)}
+          </VStack>
+        </VStack>
+      </Box>
+    </Center>;
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <Center flex={1} px="3">
+                <Example />
+            </Center>
+    </NativeBaseProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
